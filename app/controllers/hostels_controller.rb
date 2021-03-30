a12b458119249f26f9bf663baab6336cf3ea8d54
oomiya karen <krn971138@gmail.com>
2021-03-30 21:14:38 +0900

    set hostels index

--- app/controllers/hostels_controller.rb
+++ app/controllers/hostels_controller.rb
@@ -2,6 +2,7 @@ class HostelsController < ApplicationController
   before_action :set_hostel, only: %i[ show edit update destroy ]
 
   def index
+    # binding.irb
     @q = Hostel.ransack(params[:q])
     @hostels = @q.result(distinct: true)
   end
@@ -13,10 +14,12 @@ class HostelsController < ApplicationController
   def set_hostel
     @hostel = Hostel.find(params[:id])
   end
+  
 
   # Only allow a list of trusted parameters through.
   def hostel_params
     params.require(:hostel).permit(:name, :contentname, :address, :kind, :walk_city_time, :price,
-    :image, :phone_number, :details, :country, :capacity, :latitude, :longitude, :around_information)
+                                   :image, :phone_number, :details, :country, :capacity, 
+                                   :latitude, :longitude, :around_information)
   end
 end
