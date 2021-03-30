4ecfc5c39d240e64e42517e490b52cbafb92991c
oomiya karen <krn971138@gmail.com>
2021-03-30 15:15:50 +0900

    hostelテーブルのname,countryカラムにindexを貼る

--- .history/config/routes_20210330112442.rb
+++ .history/config/routes_20210330112442.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :hostels
+  get 'hostels/index'
+  get 'hostels/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_023935) do
+ActiveRecord::Schema.define(version: 2021_03_30_060433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_03_30_023935) do
     t.text "around_information"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name", "country"], name: "index_hostels_on_name_and_country"
   end
 
 end
