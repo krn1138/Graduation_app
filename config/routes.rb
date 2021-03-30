58c2a4af04dd3473770bbc1abc7f5f47e9b89342
oomiya karen <krn971138@gmail.com>
2021-03-30 21:20:54 +0900

    set hostels/search show route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :hostels
   root "hostels#index"
+
+  # MARK: hostels routings 
+  resources :hostels
+  namespace :hostels do
+    get '/search/', to: 'search#show'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
