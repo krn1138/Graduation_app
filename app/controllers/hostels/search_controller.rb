16c28aae8187198d987340c95946f64912823474
oomiya karen <krn971138@gmail.com>
2021-03-31 19:07:48 +0900

    ransack/name,countryカラムで宿検索可能

--- app/controllers/hostels/search_controller.rb
+++ app/controllers/hostels/search_controller.rb
@@ -2,5 +2,7 @@ class Hostels::SearchController < ApplicationController
     def show
         @hostel = Hostel.new
         @q = Hostel.ransack(params[:q])
+
+
     end
 end
--- app/controllers/hostels_controller.rb
+++ app/controllers/hostels_controller.rb
@@ -2,8 +2,18 @@ class HostelsController < ApplicationController
   before_action :set_hostel, only: %i[ show edit update destroy ]
 
   def index
-    @q = Hostel.ransack(params[:q])
-    @hostels = @q.result(distinct: true)
+
+    if params[:q] != nil
+      # binding.irb
+      params[:q]['name_or_country_cont_any'] = params[:q]['name_or_country_cont_any'].to_s.split(/[\p{blank}\s]+/)
+      @q = Hostel.ransack(params[:q])
+      @hostels = @q.result(distinct: true)
+    else
+      @q = Hostel.ransack(params[:q])
+      @hostels = @q.result(distinct: true)
+    end
+    # @q = Hostel.ransack(params[:q])
+    # @hostels = @q.result(distinct: true)
   end
 
   def show
