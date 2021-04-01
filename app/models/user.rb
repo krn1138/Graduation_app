bb9e31ec7bc046e967fadf0498322d507c99ffa5
oomiya karen <krn971138@gmail.com>
2021-04-01 14:11:05 +0900

    devise/user 新規登録画面でゲスト、ホスト選択表示実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: {
+    ゲスト:1, ホスト:2
+  }
 end
