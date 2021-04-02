c66f1a34774c032c44079ed432f3c9c31bfcba13
oomiya karen <krn971138@gmail.com>
2021-04-02 12:08:17 +0900

    guest,host選択後、それぞれ画面遷移 RegistrationsControllerで条件分岐

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -51,15 +51,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  #   binding.irb
-  #   if resource[:role] == "ゲスト"
-  #     new_guest_path
-  #   else
-  #     new_host_path
-  #   end
-  # end
+  def after_sign_up_path_for(resource)
+    super(resource)
+    binding.irb
+    if resource[:role] == "ゲスト"
+      new_guest_path
+    else
+      new_host_path
+    end
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
