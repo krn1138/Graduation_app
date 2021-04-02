0f1113fe75422528d1b75d6714067cc5fb964213
oomiya karen <krn971138@gmail.com>
2021-04-02 12:06:00 +0900

    user.rb dependent: :destroy実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
-  # has_one :host, dependent: :destroy
-  # has_one :guest, dependent: :destroy
-  has_one :host
-  has_one :guest
+  has_one :host, dependent: :destroy
+  has_one :guest, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
