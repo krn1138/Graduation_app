171720f65900bd85b6b34b99364db6d4f26ee62e
oomiya karen <krn971138@gmail.com>
2021-04-06 14:38:49 +0900

    user_controller,hostel_controller/before_action実装

--- app/controllers/hostels_controller.rb
+++ app/controllers/hostels_controller.rb
@@ -1,6 +1,7 @@
 class HostelsController < ApplicationController
   before_action :set_hostel, only: %i[ show edit update destroy ]
   before_action :set_search_hostel
+  before_action :hostel_registration, only: [:new, :create]
 
   def index
   end
@@ -10,13 +11,21 @@ class HostelsController < ApplicationController
   end
 
   def create
-    @hostel = hostel.build_host(hostel_params)
+    # @hostel = host.build_hostel(hostel_params)
+    @hostel = Hostel.new(hostel_params)
+    @hostel.host_id = current_user.host.id
 
     if @hostel.save
       redirect_to hostels_path, notice: "宿を登録しました！"
     else
       render :new
     end
+
+    # if @hostel.build_host.save
+    #   redirect_to hostels_path, notice: "宿を登録しました！"
+    # else
+    #   render :new
+    # end
   end
 
   def show
@@ -34,5 +43,11 @@ class HostelsController < ApplicationController
     params.require(:hostel).permit(:name, :contentname, :address, :kind, :walk_city_time, :price,
                                    :image, :phone_number, :details, :country, :capacity, 
                                    :latitude, :longitude, :around_information)
+  end
+
+  def hostel_registration
+    unless current_user.host
+      redirect_to root_path, notice: "ホスト権限はありません"
+    end
   end
 end
