62322d25332914cfad67fcb2f1265b94066cfbc3
oomiya karen <krn971138@gmail.com>
2021-04-29 18:24:32 +0900

    fix: 電話番号バリデーション追加

--- app/models/hostel.rb
+++ app/models/hostel.rb
@@ -5,7 +5,16 @@ class Hostel < ApplicationRecord
   #   enum kind: {
   #     single: 0, twin: 1, dormitory: 2
   # }
+  validates :phone_number,  presence: true, length: { maximum: 11 },
+                            format: { with: /\A[0-9]+\z/ }
 
+  validate :validate_images
+  private
+  def validate_images
+    num = images.count
+    return if  num >= 1 && num <= 5
+    errors.add(:images, 'は1ファイル以上5ファイル以下でアップロードしてください')
+  end
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 
